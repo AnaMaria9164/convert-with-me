b2b841870a1ddc624c490f1d32c82c0d87e66494
Iacob Ana Maria <ana.mm9164@gmail.com>
2023-03-30 18:35:16 +0100

    updated the currencyRow.js file

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -8,13 +8,18 @@ export default function CurrencyRow(props) {
     onChangeAmount,
     amount
   }= props
+
+  const options = []
+
+  currencyOptions.forEach((option,idx) => {
+    options.push(<option key={idx} value={option}>{option}</option>)
+  })
+
   return (
     <div>
-      <input type="number" className="input" value = {amount} onChange={onChangeAmount}/>
-      <select value={selectedCurrency}onChange={onChangeCurrency}>
-        {currencyOptions.map(option => {
-        <option key={option} value={option}>{option}</option>
-        })}
+      <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {options}
         </select> 
     </div>
   )
