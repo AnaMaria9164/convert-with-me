db6d32f7351922c26cc9713727b298e897894aee
Iacob Ana Maria <ana.mm9164@gmail.com>
2023-03-30 18:29:43 +0100

    updated the html file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow'
 
-const Base_URL = 'http://api.exchangeratesapi.io/latest'
+const baseUrl = 'https://api.apilayer.com/exchangerates_data/latest'
 function App() {
   const[currencyOptions, setCurrencyOptions]=useState([])
   const[fromCurrency, setFromCurrency] = useState()
@@ -14,36 +14,60 @@ function App() {
   let toAmount, fromAmount
   if (amountInFromCurrency) {
     fromAmount = amount
-    toAmount = amount * exchangeRate
+    if( typeof exchangeRate != 'undefined') {
+      toAmount = amount * exchangeRate
+    } else {
+      toAmount = amount
+    }
+
   } else {
     toAmount = amount
-    fromAmount = amount / exchangeRate
+    if( typeof exchangeRate != 'undefined') {
+      fromAmount = amount / exchangeRate
+    } else {
+      fromAmount = amount
+    }
   }
 
 useEffect(()=> {
-fetch(Base_URL)
-.then(res => res.json())
-.then(data => {
-  const firstCurrency = Object.keys(data.rates)[0]
-  setCurrencyOptions([data.base, ...Object.keys(data.rates)])
-  setFromCurrency(data.base)
-  setToCurrency(firstCurrency)
-  setExchangeRate(data.rates[firstCurrency])
-   
-})
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "QGJmserWcZdPHYiNhg1UDm6GTCoB0N4E");
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+  fetch(baseUrl,requestOptions)
+    .then(res => res.json())
+    .then(data => {
+      const firstCurrency = Object.keys(data.rates)[0]
+      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      setFromCurrency(data.base)
+      setToCurrency(firstCurrency)
+      setExchangeRate(data.rates[firstCurrency])
+    })
 }, [])
 
 useEffect(() => {
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "QGJmserWcZdPHYiNhg1UDm6GTCoB0N4E");
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
   if (fromCurrency != null && toCurrency != null) {
-    fetch(`${Base_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+    fetch(`${baseUrl}?base=${fromCurrency}&symbols=${toCurrency}`, requestOptions)
     .then(res => res.json())
     .then(data => setExchangeRate(data.rates[toCurrency]))
   }
 },[fromCurrency, toCurrency])
 
 function handleFromAmountChange(e) {
-setAmount(e.target.value)
-setAmountInFromCurrency(true)
+  setAmount(e.target.value)
+  setAmountInFromCurrency(true)
 }
 
 function handleToAmountChange(e) {
@@ -53,14 +77,13 @@ function handleToAmountChange(e) {
 
   return (
     <>
-      <h1>Convert with me</h1>
+      <h1>Convert app</h1>
       <CurrencyRow
       currencyOptions={currencyOptions}
       selectedCurrency={fromCurrency}
       onChangeCurrency={e => setFromCurrency(e.target.value)}
       onChangeAmount={handleFromAmountChange}
       amount = {fromAmount}
-
       />
       <div className="equals"> = </div>
       <CurrencyRow
